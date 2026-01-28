69cd3c0f354b1bb1066eaea2dacdeccdd1eb7f57
vaishnavimatre <146953160+vaishnavimatre@users.noreply.github.com>
2026-01-28 17:38:31 +0530

    Create sqlfileformat.sql

---
+++ sqlfileformat.sql
@@ -0,0 +1,8 @@
+IF NOT EXISTS (SELECT * FROM sys.external_file_formats WHERE name = 'SynapseDelimitedTextFormat') 
+CREATE EXTERNAL FILE FORMAT [SynapseDelimitedTextFormat] 
+WITH ( FORMAT_TYPE = DELIMITEDTEXT ,
+FORMAT_OPTIONS (
+FIELD_TERMINATOR = ',',
+USE_TYPE_DEFAULT = FALSE
+))
+GO
